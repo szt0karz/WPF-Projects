00ea52c04ab1d6514cf7c5c7a01cf1b528716c20
szt0karz <szymon.y.pl@gmail.com>
2025-02-23 11:18:59 +0100

    Add files via upload

---
+++ Notatnik/Notatnik/InsertDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace Notatnik
+{
+    /// <summary>
+    /// Logika 
+    /// </summary>
+    public partial class InsertDialog : Window
+    {
+        public InsertDialog()
+        {
+            InitializeComponent();
+        }
+
+        // Obsługuje kliknięcie przycisku "Wstaw"
+        private void Accept(object sender, RoutedEventArgs e)
+        {
+            // Ustawienie dialogu jako zakończonego sukcesem
+            DialogResult = true;
+            // Zamknięcie okna dialogowego
+            Close();
+        }
+    }
+}
---
+++ Notatnik/Notatnik/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Notatnik
+{
+    /// <summary>
+    /// Logika
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog openDialog = new OpenFileDialog();  // Dialog do otwierania pliku
+        SaveFileDialog saveDialog = new SaveFileDialog();  // Dialog do zapisywania pliku
+
+        public MainWindow()
+        {
+            InitializeComponent();  
+        }
+
+        // Obsługuje kliknięcie przycisku "Nowa notatka"
+        private void NewNote(object sender, RoutedEventArgs e)
+        {
+            // Pokazuje komunikat z pytaniem, czy użytkownik chce rozpocząć nową notatkę
+            if (MessageBox.Show("Czy nowa notatka?", "Nowa", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                Notepad.Clear();  // Czyści zawartość TextBox
+            }
+        }
+
+        // Obsługuje kliknięcie przycisku "Zapisz notatkę"
+        private void SaveNote(object sender, RoutedEventArgs e)
+        {
+            // Pokazuje dialog zapisu pliku
+            if (saveDialog.ShowDialog() == true)
+            {
+                // Zapisuje tekst z TextBoxa do wybranego pliku
+                File.WriteAllText(saveDialog.FileName, Notepad.Text);
+            }
+        }
+
+        // Obsługuje kliknięcie przycisku "Otwórz notatkę"
+        private void OpenNote(object sender, RoutedEventArgs e)
+        {
+            // Pokazuje dialog otwierania pliku
+            if (openDialog.ShowDialog() == true)
+            {
+                // Wczytuje zawartość wybranego pliku do TextBoxa
+                Notepad.Text = File.ReadAllText(openDialog.FileName);
+            }
+        }
+
+        // Obsługuje kliknięcie przycisku "Wstaw symbol"
+        private void InsertSymbol(object sender, RoutedEventArgs e)
+        {
+            InsertDialog insertDialog = new InsertDialog();  // Tworzy nowe okno dialogowe
+
+            // Modalne: czeka na akcję użytkownika w oknie dialogowym
+            if (insertDialog.ShowDialog() == true)
+            {
+                // Wstawia wybrany symbol do aktualnie wybranego tekstu
+                Notepad.SelectedText = insertDialog.Symbol.Text;
+            }
+
+        }
+    }
+}
